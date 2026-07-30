a41c53e03fc5538e0431c2871641de11d38541b5
PouplarWesel <94089618+PouplarWesel@users.noreply.github.com>
2026-07-29 20:08:37 -0700

    Battery Sim Configs

--- Vehicle/Battery/Configs/battery1.py
+++ Vehicle/Battery/Configs/battery1.py
@@ -1,5 +1,11 @@
-from Vehicle.Battery.BatteryConfig import BatteryConfig #TODO: Rename battery1.py to the correct name
+from Vehicle.Battery.BatteryConfig import BatteryConfig
 
 battery1 = BatteryConfig(
     mass_kg=50.8,
+    series_cells=140,
+    parallel_cells=3,
+    cell_capacity_ah=5.0,
+    cell_nominal_voltage_v=3.6,
+    cell_internal_resistance_ohm=6.7e-3,
+    cell_max_discharge_current_a=125.0,
 )

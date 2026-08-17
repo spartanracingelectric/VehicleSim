bf51c55a93d9aa77653c3e9a1cfd7fa6fdc694d5
PouplarWesel <94089618+PouplarWesel@users.noreply.github.com>
2026-08-17 02:46:19 -0700

    Updating configs

--- Vehicle/Battery/Configs/battery2.py
+++ Vehicle/Battery/Configs/battery2.py
@@ -1,6 +1,6 @@
-from Vehicle.Battery.Battery import Battery
+from Vehicle.Battery.Pack import Pack
 
-battery2 = Battery(
+battery2 = Pack(
     mass_kg=45.8,
     initial_soc=1.0,
     initial_temp_C=25.0,
--- Vehicle/Battery/Configs/battery3.py
+++ Vehicle/Battery/Configs/battery3.py
@@ -1,6 +1,6 @@
-from Vehicle.Battery.Battery import Battery
+from Vehicle.Battery.Pack import Pack
 
-battery3 = Battery(
+battery3 = Pack(
     mass_kg=55.8,
     initial_soc=1.0,
     initial_temp_C=25.0,

345e2ac7e9da940b73110a34b48409a0754cf865
Kennedy2902 <kennedynguyen2902@gmail.com>
2026-08-20 17:32:31 -0700

    comments about confusions

--- Vehicle/Driver/DriverConfig.py
+++ Vehicle/Driver/DriverConfig.py
@@ -2,12 +2,17 @@ from Functions import PIDcontroller
 
 class DriverConfig: #TODO: Add parameters related to driver
     def __init__(self):
-        self.pid = PIDcontroller(10, 10, 0, 231, 10)
+        self.pid = PIDcontroller(10, 10, 0, 80, 10)
         self.pedalPosition = 0
         self.targetSpeed = 0
         self.thresholdDiscrepancy = 5
         self.initializationThreshold = 0
         self.clampingMethod = 0
+
+    # how to accurately tune PID values? how to determine the saturation value?
+    # where is target speed being read from?
+    # where is current speed being read from?
+    # how to associate the pedal position with speed?
 
     def updatePIDController(self, pidSetpoint, sensorValue):
         currentError = pidSetpoint - sensorValue

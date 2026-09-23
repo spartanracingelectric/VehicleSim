f3b227412beae30fc0b1e5a0ac4c02b0cf1473e3
jailynyun <jailynyun@gmail.com>
2026-09-22 19:06:29 -0700

    constants

--- Vehicle/Motor/Configs/emrax228_mv.py
+++ Vehicle/Motor/Configs/emrax228_mv.py
@@ -7,3 +7,11 @@ emrax228_mv = MotorConfig(
     peak_power_W=124000,
     max_rpm=6500
 )
+
+amk_dd5 = MotorConfig(
+    Kt = 0.26,
+    peakCurrent_A = 105,
+    peakTorque_Nm = 21,
+    peak_power_W = 12300, #rated power, not true peak power
+    max_rpm = 20000
+) 

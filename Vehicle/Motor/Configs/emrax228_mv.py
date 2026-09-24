8ad4f699ce68e34b9bbe5cbee8f2b75853dfefd1
jailynyun <jailynyun@gmail.com>
2026-09-23 22:17:35 -0700

    actual constants needed

--- Vehicle/Motor/Configs/emrax228_mv.py
+++ Vehicle/Motor/Configs/emrax228_mv.py
@@ -1,19 +1,27 @@
 from Vehicle.Motor.MotorConfig import MotorConfig
 
 # old constants
-emrax228_mv = MotorConfig(
-    Kt=0.64,
-    peakCurrent_A=360,
-    peakTorque_Nm=220,
-    peak_power_W=124000,
-    max_rpm=6500
-)
+# emrax228_mv = MotorConfig(
+#     Kt=0.64,
+#     peakCurrent_A=360,
+#     peakTorque_Nm=220,
+#     peak_power_W=124000,
+#     max_rpm=6500
+# )
 
 # new constants
 amk_dd5 = MotorConfig(
+    # Kt = 0.26,
+    # stator_resistance_ohm = 0.135,
+    # num_poles = 10, #poles
+    # quadrature_axis_inductance_H = 0.00012, #millihenries (DS) -> Henries
+    # direct_axis_inductance_H = 0.00024, 
+    # rotor_intertia_kgm2 = 0.000274 #kgcm^2 (DS) ->kgm^2
+
     Kt = 0.26,
-    peakCurrent_A = 105,
-    peakTorque_Nm = 21,
-    peak_power_W = 12300, #rated power, not true peak power
-    max_rpm = 20000
+    Rs = 0.135, #stator winding resistance
+    Np = 10, #number of poles
+    Lq = 0.00012, #quadrature axis inductance (H)
+    Ld = 0.00024, #direct axis inductance (H)
+    Jm = 0.000274 #inertia (kgm^2)
 ) 
